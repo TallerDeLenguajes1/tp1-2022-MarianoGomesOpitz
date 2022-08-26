d91219f387fccccf80b3386bf62857d6021c164c
MarianoGomesOpitz <marianogomesopitz@gmail.com>
2022-08-26 19:27:12 -0300

    Ejercicio 2 V3

--- Ejercicio2/Program.cs
+++ Ejercicio2/Program.cs
@@ -1,12 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
+using System.IO;
+using System.Net;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace Ejercicio2 // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
         static void Main(string[] args)
-        {
+        {/*
             //Problema 1: Cuadrado de un número
             Console.WriteLine("\n\n/----------------CUADRADO----------------/");
             Console.WriteLine("\nIngresar un número para obtener un cuadrado");
@@ -65,8 +69,98 @@ namespace Ejercicio2 // Note: actual namespace depends on the project name.
             {
                 Console.WriteLine($"\n{x}\nError detectado, uno de los números asignados es muy grande");
             }
+*/
+            //Problema 4: Consumo de API
+            Console.WriteLine("\n\n/----------------CONSUMO DE API----------------/");
 
-            //Problema 4:
+            try
+            {
+                var url = $"https://apis.datos.gob.ar/georef/api/provincias?campos=id,nombre";
+                var request = (HttpWebRequest)WebRequest.Create(url);
+                request.Method = "GET";
+                request.ContentType = "application/json";
+                request.Accept = "application/json";
+
+                Root info = null;
+
+                using (WebResponse response = request.GetResponse())
+                {
+                    using (Stream strReader = response.GetResponseStream())
+                    {
+                        if (strReader != null)
+                        {
+                            using (StreamReader objReader = new StreamReader(strReader))
+                            {
+                                string texto = objReader.ReadToEnd();
+
+                                info = JsonSerializer.Deserialize<Root>(texto);
+                            }
+                        }
+                    }
+                }
+
+                if (info != null)
+                {
+                    Mostrar(info);
+                }
+            }
+            catch (NotSupportedException)
+            {
+                Console.WriteLine($"\nError detectado");
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine($"\nError detectado");
+            }
+            catch (System.Security.SecurityException)
+            {
+                Console.WriteLine($"\nError detectado");
+            }
+            catch (UriFormatException)
+            {
+                Console.WriteLine($"\nError detectado");
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine($"\nError detectado");
+            }
+            catch (ProtocolViolationException)
+            {
+                Console.WriteLine($"\nError detectado");
+            }
+            catch (WebException)
+            {
+                Console.WriteLine($"\nError detectado");
+            }
+            catch (InvalidOperationException)
+            {
+                Console.WriteLine($"\nError detectado");
+            }
+            catch (OutOfMemoryException)
+            {
+                Console.WriteLine($"\nError detectado");
+            }
+            catch (IOException)
+            {
+                Console.WriteLine($"\nError detectado");
+            }
+            catch (JsonException)
+            {
+                Console.WriteLine($"\nError detectado");
+            }
+            catch (NullReferenceException)
+            {
+                Console.WriteLine($"\nError detectado");
+            }
+        }
+
+        private static void Mostrar(Root info)
+        {
+            for (int i = info.Inicio; i < info.Cantidad; i++)
+            {
+                Console.WriteLine($"\nId: {info.Provincias[i].Id}");
+                Console.WriteLine($"Provincia: {info.Provincias[i].Nombre}");
+            }
         }
     }
 }
